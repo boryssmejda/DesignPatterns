ffb01ebf00a4d5a6ccc4e405c092aed7a8dbbf74
borys <borys.smejda@gmail.com>
2019-12-22 20:57:44 +0100

    Patterns squezzed into 3 different types

--- Strategy/Dynamic/Helicopter.cpp
+++
@@ -1,9 +0,0 @@
-#include "Helicopter.hpp"
-#include <iostream>
-
-
-void Helicopter::display()
-{
-    std::cout << "Hi,I am helicopter and my name is " << m_modelName << "\n";
-}
-
--- Strategy/Static/FighterJet.cpp
+++
@@ -1,8 +0,0 @@
-#include "FighterJet.hpp"
-#include <iostream>
-
-
-void FighterJet::display()
-{
-    std::cout << "Hi,I am FighterJet and my name is " << m_modelName << "\n";
-}
