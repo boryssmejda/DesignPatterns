2f0a0d590afc8a3e07020c8ef17e1e152c75088c
borys <borys.smejda@gmail.com>
2019-12-22 19:22:31 +0100

    Strategy pattern divided into static and dynamic version

---
+++ Strategy/Static/Helicopter.hpp
@@ -0,0 +1,16 @@
+#ifndef HELICOPTER_HPP
+#define HELICOPTER_HPP
+
+#include "FlyingObject.hpp"
+
+class Helicopter : public FlyingObject<MachineGunAttack,VerticalStart>
+{   
+    public:
+    Helicopter(double t_wingspan, int t_mass, int t_topSpeed, std::string t_model): 
+        FlyingObject(t_wingspan, t_mass, t_topSpeed, t_model)
+    {}
+
+    void display() override;
+};
+
+#endif
