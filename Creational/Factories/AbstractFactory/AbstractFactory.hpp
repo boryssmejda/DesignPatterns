3d8726eb746978edf1771ea4efca108142e09621
borys <borys.smejda@gmail.com>
2020-03-14 17:49:06 +0100

    Added virtual destructor

--- Creational/Factories/AbstractFactory/AbstractFactory.hpp
+++ Creational/Factories/AbstractFactory/AbstractFactory.hpp
@@ -3,17 +3,20 @@
 
 #include <memory>
 #include <string>
+#include <iostream>
 
 class IPlane
 {
 public:
 	virtual std::string getName() = 0;
+	virtual ~IPlane() { std::cout << "IPlane destroyed\n"; }
 };
 
 class ITank
 {
 public:
 	virtual std::string getName() = 0;
+	virtual ~ITank() { std::cout << "ITank destroyed\n"; }
 };
 
 
@@ -27,7 +30,8 @@ public:
 
 	virtual std::unique_ptr<IPlane> getPlane(std::string t_planeName) = 0;
 	virtual std::unique_ptr<ITank> getTank(std::string t_tankName) = 0;
-
+	virtual ~MilitaryEquipmentFactory() { std::cout << "Factory destroyed\n"; };
+	
 	static std::unique_ptr<MilitaryEquipmentFactory> createFactory(EquipmentFactory t_factory);
 };
 
